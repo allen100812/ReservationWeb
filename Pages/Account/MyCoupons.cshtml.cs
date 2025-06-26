5a9c2b3a7607fefcfca5bbe8dffb5fd6c0b61ab5
Colin Miao <allen100812@gmail.com>
2025-06-26 15:18:26 +0800

    我的優惠券前端完成

--- Pages/Account/MyCoupons.cshtml.cs
+++ Pages/Account/MyCoupons.cshtml.cs
@@ -8,10 +8,12 @@ namespace Web0524.Pages.Account
     public class MyCouponsModel : PageModel
     {
         private readonly IMarketingService _marketingService;
+        private readonly IUserService _userService;
 
-        public MyCouponsModel(IMarketingService marketingService)
+        public MyCouponsModel(IMarketingService marketingService, IUserService userService)
         {
             _marketingService = marketingService;
+            _userService = userService;
         }
 
         public List<CouponDispatchRecord> DispatchRecords { get; set; } = new();
@@ -30,6 +32,14 @@ namespace Web0524.Pages.Account
 
         public JsonResult OnGetGenerateQR(int recordId)
         {
+
+            var pageName = this.GetType().Name.Replace("Model", "");
+            var userId = User.FindFirst(System.Security.Claims.ClaimTypes.Sid)?.Value;
+            if (string.IsNullOrEmpty(userId) || !_userService.HasPagePermissionByName(userId, pageName))
+            {
+                return new JsonResult(new { success = false, message = "µLÅv­­" });
+            }
+
             var qr = _marketingService.GenerateCouponQRCode(recordId);
             return new JsonResult(new { qr });
         }
